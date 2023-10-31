f0daed05afad1dde04dbe107af62ab5e3ffdaa61
Xwhut <87642734+Xwhut@users.noreply.github.com>
2023-10-31 15:45:35 +0800

    Add files via upload

--- example/The_three-point_estimation_method.py
+++ example/The_three-point_estimation_method.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from time import time
+import networkx as nx
+import matplotlib.pyplot as plt
 import os
 import psutil
 itime = time()
@@ -243,7 +244,31 @@ df.index = yticklabels
 df.columns = xticklabels
 df.index.name = 'cell_type'
 df.to_csv(cancer + "\\case_study.csv", index=True)
-plt.savefig(cancer + '\\case_study.pdf', dpi=1080,bbox_inches = 'tight')   #
+plt.savefig(cancer + '\\case_study.pdf', dpi=1080,bbox_inches = 'tight')
 print("-----CellDialog Run Completed----")
-# print(' Time: {}秒, mem: {}MB'.format((time() - itime), psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024))
 plt.show()
+
+#  network view
+df = pd.read_csv(cancer + "\\case_study.csv", index_col=0)
+matrix = df.values.tolist()
+a = df.shape[0]
+node_colors = ['hotpink', 'darkorange', 'b', 'y', 'm', 'c', 'gray', 'violet', 'r', 'khaki', 'darkred', 'saddlebrown'][:a]
+G = nx.DiGraph()
+for i, name in enumerate(df.index):
+    G.add_node(name, node_color=node_colors[i % len(node_colors)])
+for i, row in enumerate(matrix):
+    for j, weight in enumerate(row):
+        if weight != 0:
+            G.add_edge(df.index[i], df.columns[j], weight=weight, edge_color=node_colors[i % len(node_colors)])
+pos = nx.circular_layout(G)
+edge_widths = [6 * d['weight'] for u, v, d in G.edges(data=True)]
+edge_colors = [d['edge_color'] for u, v, d in G.edges(data=True)]
+node_colors = [d['node_color'] for n, d in G.nodes(data=True)]
+
+nx.draw_networkx_edges(G, pos, width=edge_widths, alpha=0.8, edge_color=edge_colors, arrowsize=14, node_size=300, connectionstyle='arc3,rad=0.1')
+nx.draw_networkx_nodes(G, pos, node_shape='o', node_color=node_colors, node_size=250)
+nx.draw_networkx_labels(G, pos, font_size=12, font_family='sans-serif')
+plt.axis('off')
+# plt.savefig('melanoma\\case_study.pdf', dpi=1080,bbox_inches = 'tight')
+plt.show()
+# print(' Time: {}秒, mem: {}MB'.format((time() - itime), psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024))
